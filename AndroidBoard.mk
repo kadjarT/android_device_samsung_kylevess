354487d43e22f3367222c4d12e95d403c69d5fa6
h2o64 <theh2o64@gmail.com>
2014-10-22 09:36:19 +0200

    Mak things proper

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -6,3 +6,33 @@ ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
 -include vendor/samsung/kyleve/AndroidBoardVendor.mk
+
+# Init files
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/init.hawaii_ss_kyleve.rc:root/init.hawaii_ss_kyleve.rc \
+device/samsung/kyleve/init.bcm2166x.usb.rc:root/init.bcm2166x.usb.rc \
+device/samsung/kyleve/init.bt.rc:root/init.bt.rc \
+device/samsung/kyleve/init.log.rc:root/init.log.rc \
+device/samsung/kyleve/init.recovery.kyleve.rc:root/init.recovery.kyleve.rc \
+device/samsung/kyleve/lpm.rc:root/lpm.rc \
+device/samsung/kyleve/ueventd.hawaii_ss_kyleve.rc:root/ueventd.hawaii_ss_kyleve.rc \
+device/samsung/kyleve/fstab.hawaii_ss_kyleve:root/fstab.hawaii_ss_kyleve \
+
+# Prebuilt kl keymaps
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/bcm_headset.kl:system/usr/keylayout/bcm_headset.kl \
+device/samsung/kyleve/bcm_keypad_v2.kl:system/usr/keylayout/bcm_keypad_v2.kl \
+device/samsung/kyleve/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
+device/samsung/kyleve/samsung-keypad.kl:system/usr/keylayout/samsung-keypad.kl
+
+# Prebuilt kcm keychars
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/bcm_keypad_v2.kcm:system/usr/keychars/bcm_keypad_v2.kcm
+
+# Prebuilt icu
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/icudt48l.dat:system/usr/icu/icudt48l.dat
+
+# Prebuilt idc
+PRODUCT_COPY_FILES += \
+device/samsung/kyleve/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
