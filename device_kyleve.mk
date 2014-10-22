fefb3e0f2dceda8a5d0b3e197b9a0c45ef5d479b
h2o64 <theh2o64@gmail.com>
2014-10-22 09:40:41 +0200

    Update DPI display

--- device_kyleve.mk
+++ device_kyleve.mk
@@ -1,15 +1,34 @@
+#
+# Copyright (C) 2014 The AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+#Vendor Files
 $(call inherit-product-if-exists, vendor/samsung/kyleve/kyleve-vendor.mk)
 
-PRODUCT_NAME := full_kyleve
-PRODUCT_DEVICE := kyleve
-
 # Use high-density artwork where available
-PRODUCT_LOCALES += hdpi
+# Screen density
+PRODUCT_AAPT_CONFIG := hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
 
 DEVICE_PACKAGE_OVERLAYS += device/samsung/kyleve/overlay
 
@@ -76,7 +95,7 @@ PRODUCT_PACKAGES += \
 	libstagefrighthw \
 	libSEC_OMX_Resourcemanager \
 	libSEC_OMX_Core \
-		libOMX.SEC.AVC.Decoder \
+	ibOMX.SEC.AVC.Decoder \
 	libOMX.SEC.M4V.Decoder \
 	libOMX.SEC.WMV.Decoder \
 	libOMX.SEC.AVC.Encoder \
@@ -116,7 +135,8 @@ include frameworks/native/build/phone-hdpi-512-dalvik-heap.mk
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+#Classic
 $(call inherit-product, build/target/product/full.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
